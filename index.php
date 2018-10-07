62f0c6b0346737a5d93b17a1d99cf2c70dd108de
Jiah <itisgia@gmail.com>
2018-10-08 09:44:15 +1300

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+// You can write php on the top midle or bottom. If you wrtie it on the top, It will render first.
+#This is another single line comment
+
+/*
+M
+U
+L
+T
+I Line comment */
+
+// in php var is $
+$hello = "HELLO";
+
+$_hello ="Hello with underscore";
+
+//semi colons in php is really important
+
+$number = 10;
+$boolean = false;
+
+$array = array(10,20,30,40);
+
+//echo is spiitng out data, information
+echo "Hello";
+echo ("Hello with bracket");
+echo $_hello;
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+    <body  style="font-family: 'Roboto', sans-serif;">
+        <h1>Learning PHP</h1>
+    </body>
+</html>
